3180828c5507a174ab840375037d58c1e3ffb5ea
rjabhi <rjabhi@amazon.com>
2024-12-30 15:18:21 -0800

    init commit

---
+++ amplify/backend/function/testfn/src/index.js
@@ -0,0 +1,17 @@
+import {v7} from 'uuid';
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: JSON.stringify(`Hello from Lambda!: ${v7({msecs: 2 })}`),
+    };
+};
